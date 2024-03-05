120935b94847a00081d8737b35992747fdf4d89e
Dean Freeman <dean.freemanwilde@gmail.com>
2024-03-05 11:17:57 -0500

    Added settings.py
    Contains class Settings to initalize game settings

--- alien_invasion/alien_invation.py
+++ alien_invasion/alien_invation.py
@@ -2,18 +2,23 @@ import sys
 import pygame as pg
 
 class AlienInvasion:
-    """Overall class to manage game assets and behavior.
+    """
+    Overall class to manage game assets and behavior.
     """
 
     def __init__(self) -> None:
-        """Initialize the game, and create the game resources
+        """
+        Initialize the game, and create the game resources
         """
         pg.init()
     
         self.screen = pg.display.set_mode((1200, 800))   
         pg.display.set_caption("Alien Invasion")
+
+        # Set background color
+        self.bg_color = (230, 230, 230)
     
-    def run_game(self):
+    def run_game(self) -> None:
         """
         Start the main loop for the game.
         """
@@ -22,6 +27,9 @@ class AlienInvasion:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
+            
+            # Redraw the screen durring each pass through the loop
+            self.screen.fill(self.bg_color)
             
             # Make the most recently drawn screen visible.
             pg.display.flip()
