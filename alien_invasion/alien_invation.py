0450b07cf18a4e1cb0dde8d07760b1a0fae71c8f
Dean Freeman <dean.freemanwilde@gmail.com>
2024-03-11 09:54:59 -0400

    alien_invasion.py replaced windowed screen with fullscreen

--- alien_invasion/alien_invation.py
+++ alien_invasion/alien_invation.py
@@ -37,15 +37,38 @@ class AlienInvasion:
             if event.type == pg.QUIT:
                 sys.exit()
             elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pg.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pg.pg.KEYUP:
-                if event.key == pg.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pg.pg.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Responds to keypresses.
+
+        Args:
+            event (pg.event): pygame keypress event
+        """
+        # right arrow
+        if event.key == pg.K_RIGHT:
+            self.ship.moving_right = True
+        # left arrow
+        elif event.key == pg.K_LEFT:
+            self.ship.moving_left = True
+        # 'q'
+        elif event.key == pg.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Responds to key releases.
+
+        Args:
+            event (pg.event): pygame key release event
+        """
+        # right arrow
+        if event.key == pg.K_RIGHT:
+            self.ship.moving_right = False
+        # left arrow
+        elif event.key == pg.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
