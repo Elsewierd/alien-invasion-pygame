516196c73a0ba7dae47a0721b87768e9e7ca0f52
Dean Freeman <dean.freemanwilde@gmail.com>
2024-03-14 10:14:18 -0400

    REFACTOR
    alien_invasion.py
    _create_alien includes row_number passed from _create_fleet allowing for multiple rows of aliens

--- alien_invasion/alien_invation.py
+++ alien_invasion/alien_invation.py
@@ -97,22 +97,38 @@ class AlienInvasion():
         """
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
+    
+    def _create_alien(self, alien_number, row_number):
+        """Create an alien and place it in a row
+
+        Args:
+            alien_number (int): the number of the alien created from left to right
+            row_number (int): the number of the row the alien created in from top
+        """
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def _create_fleet(self):
-        """Creates a fleet of aliens by populating self.aliens:Group
+        """Creates a fleet of aliens
         """
         # New Alien instance
         alien = Alien(self)
         # Determine the number of aliens per row
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         availble_space_x = self.settings.screen_width - (2 * alien_width)
         aliens_per_row = availble_space_x // (2 * alien_width)
+        # Determine the number of possible rows
+        ship_height = self.ship.rect.height
+        availble_space_y = self.settings.screen_height - (3 * alien_height) - ship_height
+        number_rows = availble_space_y // (2 * alien_height)
         # Create a row of aliens
-        for alien_number in range(aliens_per_row):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            # Add to Group
-            self.aliens.add(alien)
+        for row_number in range(number_rows):
+            for alien_number in range(aliens_per_row):
+                self._create_alien(alien_number, row_number)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
